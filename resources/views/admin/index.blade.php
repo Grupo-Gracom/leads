b79e1d22e73ee2f3c1233f96e770ba66e57bc16e
WillamisRodrigues <wrodrigues153@gmail.com>
2021-10-15 18:21:16 -0300

    api de consumo leads

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,14 +5,15 @@
 <div class="leads">
         <h3>Leads</h3>
         <ul>
-            
+            <a href="admin/gracom">
             <li class="suave">
                 <figure>
                     <img src="https://fpeduc.com/img/logo-gracom.png" alt="">
                 </figure>
                 <h6>Gracom</h6>
-                <h2>-</h2>
+                <h2 id="quantidade">-</h2>
             </li>
+            </a>
             <li class="suave">
                 <figure>
                     <img src="https://fpeduc.com/img/logo-imugi.png" alt="">
@@ -36,7 +37,7 @@
         <ul>
             <li class="suave">
                 <h6>Total de Leads</h6>
-                <h2>-</h2>
+                <h2 id="total">-</h2>
             </li>
             <li class="suave">
                 <h6 class="truncate">Leads Mês Atual</h6>
@@ -49,4 +50,28 @@
         </ul>
     </div>
 </main>
+<script>
+    $(document).ready(function(){
+        var qtd = document.getElementById("quantidade").value;
+        var link = "https://gracomonline.com.br/quantidade";
+  $.ajax({
+      url: link,
+      type: 'GET',
+      dataType: 'json',
+
+    })
+    .done(function(data) {
+     var quant = JSON.parse(data);
+      document.getElementById("quantidade").innerHTML = quant;
+      document.getElementById("total").innerHTML = quant;
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+});
+</script>
 @endsection
+
